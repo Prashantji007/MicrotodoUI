433b1ac6e6ae24510fd0ed6dd13fe7aaf1e277cc
Prashantji007 <143020634+Prashantji007@users.noreply.github.com>
2025-11-06 19:12:34 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
 
 const config = {
-    GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || '/get-task',
-    DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || '/delete-task',
-    CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || '/add-task',
+    GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || 'http://4.224.131.104/get-task',
+    DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || 'http://4.224.131.104/delete-task',
+    CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || 'http://4.224.131.104/get-task',
   };
   
   export default config;
